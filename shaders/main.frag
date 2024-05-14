280e2a06eb1969ca0c8d034e2e156c8e36205f6c
overoxidize <overoxidize@protonmail.com>
2024-05-14 11:49:56 -0400

    good

--- shaders/main.frag
+++ shaders/main.frag
@@ -4,8 +4,9 @@ uniform float u_time;
 uniform sampler2D u_noise;
 uniform sampler2D u_foreground;
 uniform sampler2D u_background;
-uniform bool u_is_transition;
-uniform vec2 u_resolution;
+uniform float u_threshold;
+uniform float u_range;
+uniform float u_tyme;
 
 varying vec2 vTexCoord;
 
@@ -14,8 +15,8 @@ mat2 scale(vec2 _scale) {
 }
 
 vec2 increasingWaves( vec2 pos ) {
-  float numer = 50.0 + ( u_time / 1000.0 );
-  pos.y += ( sin( pos.y * numer ) / ( 80.0 + numer ));
+  float numer = 15.0 + ( u_time / 1000.0 );
+  pos.y += ( sin( pos.y * numer ) / ( 30.0 + numer ));
   return pos;
 }
 
@@ -23,31 +24,36 @@ vec2 rollingWaves( vec2 pos ) {
   return scale( abs( vec2 ( cos( u_time * .00005  )))) * pos;
 }
 
+vec4 transition( vec2 pos, sampler2D bg, sampler2D fg, float tyme ) {
+  vec4 res;
+
+  vec4 color1 = texture2D( bg, pos );
+  vec4 color2 = texture2D( fg, pos );
+  vec4 noise = texture2D( u_noise, fract(pos * 1.5 ));
+
+  float t = smoothstep(
+    ( u_threshold - u_range  ) / ( ( u_time - tyme ) / 2500.0 ),
+    ( u_threshold + u_range  ) / ( ( u_time  - tyme ) / 2500.0 ),
+    noise.r
+  );
+
+  res = mix( color1, color2, t  );
+
+
+  return res;
+}
+
+
 void main () {
   vec2 pos = vTexCoord;
-  vec4 color;
 
-  pos = increasingWaves( pos );
-  color = texture2D( u_foreground, pos );
+  pos = increasingWaves( vTexCoord );
 
-  if ( u_is_transition ) {
-    vec4 bg = texture2D( u_background, pos );
+  vec4 col;
 
-    float noise = texture2D(
-      u_noise,
-      fract( vTexCoord / u_time )
-    ).r;
+  col = transition( pos, u_background, u_foreground, u_tyme );
 
-    float t = smoothstep(
-      0.0 - 0.5,
-      0.0 + 0.5,
-      noise
-    );
 
-    color = mix(color, bg, t );
-
-  }
-
-  gl_FragColor = vec4( color );
-
+  gl_FragColor = vec4( col );
+ 
 }
--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 
 // html
 let timer 
+let nextChange;
 let canvas
 
 // fonts
@@ -19,14 +20,46 @@ let noiseTexture
 // booleans
 let isTransition
 
+let firstTexture
+let secondTexture
+
+let thirdTexture
+let fourthTexture
+
+let fifthTexture
+let sixthTexture
+
+let seventh
+let eighth
+
+let ninth
+let tenth
+
+let textures = []
+
+let counter = 0
+
 
 function preload() {
-  font = loadFont('fonts/cabalFont.ttf')
+  // font = loadFont('fonts/cabalFont.ttf')
+
+  firstTexture = loadImage("images/stem/ballerina.png")
+  secondTexture = loadImage("images/stem/blue_red_stem.jpg")
+
+  thirdTexture = loadImage("images/stem/glimmer_stem.jpg")
+  fourthTexture = loadImage("images/stem/pink_glimpses.png")
+
+  fifthTexture = loadImage("images/stem/predicting-the-present.png")
+  sixthTexture = loadImage("images/stem/thoughts_wb.png")
+
+  seventh = loadImage("images/stem/resistance.png")
+  eighth = loadImage("images/stem/yellow_org_collab.jpg")
+
+  ninth = loadImage("images/stem/yellow_actuality.png")
+  tenth = loadImage("images/stem/quantum-computer.png")
+
 
   noiseTexture = loadImage( 'images/noise/perlin.png' )
-  oceanTexture = loadImage( 'images/nature/oceans/red_ocean.png' )
-  treeTexture = loadImage( 'images/nature/plants/sunset_tree.jpg' )
-
   Shader = loadShader( 'shaders/stan.vert', 'shaders/main.frag' )
 
 }
@@ -35,47 +68,89 @@ function setup() {
   canvas = createCanvas( width, height, WEBGL)
 
   // text options
-  textSize( 24 )
-  textFont( font)
-  timeHeader = createP("").position( 10, 10 )
-  timeHeader.style("background-color", "white")
+  // textSize( 24 )
+  // textFont( font)
+  // timeHeader = createP("").position( 10, 10 )
+  // timeHeader.style("background-color", "white")
 
-  isTransition = false
+  textures = [ 
+    firstTexture, thirdTexture, 
+    secondTexture, fourthTexture,
+    fifthTexture, sixthTexture,
+    seventh, eighth,
+    ninth, tenth
+  ]
 
 }
 
 function draw() {
   background( 0 )
   timer = round( millis() / 1000 )
-  timeHeader.html(` time elapsed: ${ timer } seconds ` )
 
-  // ocean tide function
+  // timeHeader.html(` time elapsed: ${ timer } seconds ` )
+
   Shader.setUniform( 'u_time', millis() )
-  Shader.setUniform( 'u_resolution', [ width, height ])
-  Shader.setUniform( 'u_foreground', oceanTexture )
-  Shader.setUniform( 'u_is_transition', isTransition )
-
-  if ( timer % 4 == 0 ) {
-    isTransition = true
-    Shader.setUniform( 'u_background', treeTexture )
-    Shader.setUniform( 'u_is_transition', isTransition )
-    Shader.setUniform( 'u_noise', noiseTexture )
-  }
-  
-  shader( Shader )
-  // ocean tide function
-
-  rect( 0, 0, 0 )
-}
-
-
-function oceanTide() {
-  Shader.setUniform( 'u_time', millis() )
-  Shader.setUniform( 'u_resolution', [ width, height ])
-  Shader.setUniform( 'u_foreground', oceanTexture )
-  Shader.setUniform( 'u_background', treeTexture )
   Shader.setUniform( 'u_noise', noiseTexture )
+  Shader.setUniform( 'u_range', 0.2 )
+  Shader.setUniform( 'u_threshold', 1.0 )
+
+  if ( timer < 10 ) {
+    Shader.setUniform( 'u_background', textures[0] )
+    Shader.setUniform( 'u_foreground', textures[1] )
+  } 
+  else if ( timer < 20 ) {
+    Shader.setUniform( 'u_tyme', 10000 )
+    Shader.setUniform( 'u_background', textures[1] )
+    Shader.setUniform( 'u_foreground', textures[2] )
+  }
+  else if ( timer < 30 ) {
+    Shader.setUniform( 'u_tyme', 20000 )
+    Shader.setUniform( 'u_background', textures[2] )
+    Shader.setUniform( 'u_foreground', textures[3] )
+  }
+  else if ( timer < 40 ) {
+    Shader.setUniform( 'u_tyme', 30000 )
+    Shader.setUniform( 'u_background', textures[3] )
+    Shader.setUniform( 'u_foreground', textures[4] )
+  }
+
+  else if ( timer < 50 ) {
+    Shader.setUniform( 'u_tyme', 40000 )
+    Shader.setUniform( 'u_background', textures[4] )
+    Shader.setUniform( 'u_foreground', textures[5] )
+  }
+
+  else if ( timer < 60 ) {
+    Shader.setUniform( 'u_tyme', 50000 )
+    Shader.setUniform( 'u_background', textures[5] )
+    Shader.setUniform( 'u_foreground', textures[6] )
+  }
+
+  else if ( timer < 70 ) {
+    Shader.setUniform( 'u_tyme', 60000 )
+    Shader.setUniform( 'u_background', textures[6] )
+    Shader.setUniform( 'u_foreground', textures[7] )
+  }
+  else if ( timer < 80 ) {
+    Shader.setUniform( 'u_tyme', 70000 )
+    Shader.setUniform( 'u_background', textures[7] )
+    Shader.setUniform( 'u_foreground', textures[8] )
+  }
+  else if ( timer < 90 ) {
+    Shader.setUniform( 'u_tyme', 80000 )
+    Shader.setUniform( 'u_background', textures[8] )
+    Shader.setUniform( 'u_foreground', textures[9] )
+  }
+  else if ( timer < 90 ) {
+    Shader.setUniform( 'u_tyme', 80000 )
+    Shader.setUniform( 'u_background', textures[9] )
+    Shader.setUniform( 'u_foreground', textures[10] )
+  }
+
+
   shader( Shader )
+  rect( 0, 0, 0 )
+
 }
 
 function windowResized() {
