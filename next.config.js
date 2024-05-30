eebf06e188ec452889d1cc0cf5efb613a91d3eda
overoxidize <overoxidize@protonmail.com>
2024-05-30 08:11:12 -0400

    clean refresh

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.glsl/,
-      type: "asset/source",
-    })
-    return config
-  },
   experimental: {
     mdxRs: true,
   },
