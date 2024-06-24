83d4beb50bb30dd57178f765215adaddfc74181e
yannafaith <hello@yannafaith>
2024-06-24 07:06:01 -0400

    chktp

--- app/sketches/[path]/EditorSketch.tsx
+++ app/sketches/[path]/EditorSketch.tsx
@@ -1,9 +1,8 @@
-
+// @ts-nocheck
 
 "use client"
 import { useState, useRef, useEffect } from "react"
 import { colorsSketch as allImages } from "../../api/images"
-
 
 export default function EditorSketch({ path }) {
 
@@ -12,60 +11,81 @@ export default function EditorSketch({ path }) {
 
   const [ isMounted, setIsMounted ] = useState( false )
 
+  let font
+  let Shader 
+  let textures
+
+  let idx = 0
+  let changeEvery = 90
+
+  let seconds
+
+  let [ canvas, canvasParent ] = []
+  let [ timer, timerParent ] = []
+  let [ topSliderParent, topTimeSlider, topTimeSliderValue ] = []
+  let [ btmSliderParent, btmTimeSlider, btmTimeSliderValue ] = []
+
+  let [ topFrameBuffer, btmFrameBuffer ] = []
+
   function sketch( p5 ) {
-    let timer
-    let changeEvery = 90
-    let Shader 
-    let textures
-    let idx = 0
-    
+
     p5.preload = () => {
+      font = p5.loadFont("/fonts/cabalFont.ttf")
       Shader = p5.loadShader("/shaders/standard.vert", "/shaders/colors.frag")
       textures = allImages.map( tex => p5.loadImage(`/images/${ tex.path }`))
     }
   
     p5.setup = ( parentRef ) => {
       p5.pixelDensity(1)
-      p5.createCanvas( document.getElementById("editorParent").offsetWidth, document.getElementById("editorParent").offsetHeight, p5.WEBGL ).parent( parentRef )
+      canvasParent = document.getElementById("canvasParent")
+      p5.createCanvas( canvasParent.offsetWidth, canvasParent.offsetHeight, p5.WEBGL ).parent( parentRef )
+
+      topSliderParent = document.getElementById("topSliderParent")
+      topTimeSliderValue = p5.createP("").parent( "topSliderParent")
+      topTimeSlider = p5.createSlider( 0, 60, 1, 1 ).parent( topSliderParent )
+      topTimeSlider.size( 100 )
+
+      btmSliderParent = document.getElementById("btmSliderParent")
+      btmTimeSliderValue = p5.createP("").parent("btmSliderParent" )
+      btmTimeSlider = p5.createSlider( 0, 60, 1, 1 ).parent( btmSliderParent )
+      btmTimeSlider.size( 100 )
+
+      timerParent = document.getElementById("timerParent")
+      timer = p5.createP("").parent( timerParent )
+
+
     }
   
     p5.draw = () => {
-      timer = p5.round( p5.millis() / 1000 )
-      Shader.setUniform( "u_resolution", [ p5.width, p5.height ])
+      seconds = p5.millis() / 1000
+      timer.html(`${ p5.round( seconds ) } seconds`)
+      topTimeSliderValue.html(`${ topTimeSlider.value() }`)
+      btmTimeSliderValue.html(`${ btmTimeSlider.value() }`)
+      
+      Shader.setUniform( "u_time", seconds )
+      Shader.setUniform( "u_topTime", topTimeSlider.value() )
+      Shader.setUniform( "u_btmTime", btmTimeSlider.value() )
       Shader.setUniform( "u_background", textures[ idx ] )
-      Shader.setUniform( "u_foreground", textures[ idx + 1 ] )
-      Shader.setUniform( "u_time", p5.millis() / 1000 )
-  
-      // if ( timer < changeEvery ) {
-      //   Shader.setUniform( "u_foreground", textures[ idx + 1 ]) 
-      //   Shader.setUniform( "u_background",  textures[ idx ])
-      // }
-      // else if ( textures.length-2 > idx ) {
-      //   changeEvery += 90
-      //   idx+=1
-      //   Shader.setUniform( "u_timeout", p5.millis() )
-      // } 
-  
+      Shader.setUniform( "u_foreground", textures[ idx + 1 ])
+      Shader.setUniform( "u_resolution", [ canvasParent.offsetWidth, canvasParent.offsetHeight ])
+
       p5.shader( Shader )
-      p5.rect( 0, 0 , 0 )
+      p5.rect( 0 )
   
     }
   
     p5.windowResized = () => {
-      p5.resizeCanvas( document.getElementById("editorParent").offsetWidth, document.getElementById("editorParent").offsetHeight )
+      p5.resizeCanvas( canvasParent.offsetWidth, canvasParent.offsetHeight )
     }
 
   }
   
-
   async function InitP5( sketch, parentRef) {
     const p5 = (await import( "p5" )).default
-    const p = new p5( sketch, parentRef.current )
-    return p
+    return new p5( sketch, parentRef.current )
   }
 
-  useEffect(() => { 
-    if( !isMounted ) setIsMounted( true ) }, [])
+  useEffect(() => { if( !isMounted ) setIsMounted( true ) }, [])
 
   useEffect(() => { 
     if ( !isMounted ) return
@@ -73,10 +93,16 @@ export default function EditorSketch({ path }) {
     else mp5.remove()
   }, [ isMounted ])
 
+  const sliderCmptStyle = "w-[130px] flex justify-around items-center text-xs"
+
   return (
     <div>
-      <div className="h-[480px] w-[900px]" ref={ parentRef } id="editorParent" />
+      <div ref={ parentRef } id="canvasParent" className="h-[400px] w-full"  />
+      <div id="timerParent" className="border-b p-2 flex justify-end text-sm" />
+      <div className="flex p-2">
+        <div id="topSliderParent" className={ sliderCmptStyle } />
+        <div id="btmSliderParent" className={ sliderCmptStyle } />
+      </div>
     </div>
   )
 }
-
--- public/shaders/colors.frag
+++ public/shaders/colors.frag
@@ -8,39 +8,38 @@ uniform sampler2D u_foreground;
 
 varying vec2 vTexCoord;
 
+vec4 topColor;
+vec4 btmColor;
 vec4 color;
-vec4 colorB;
+
+uniform float u_topTime;
+uniform float u_btmTime;
+float t;
+
+vec4 sunsetColors;
 
 void main() {
 
   vec2 st = gl_FragCoord.xy/u_resolution.xy;
-  st = st.xy / 2.0;
   st.y = 1.0 - st.y;
 
-  // st *= vec2(0.9);
+  topColor = texture2D( u_background, st );
+  btmColor = texture2D( u_foreground, st );
 
-  // if ( st.y > 0.47 ) {
-  //   st.x = st.x + (cos(st.y / 3. ) / 5. ) * (sin(u_time / 50.0 ));
-  //   // st.y -= ( cos( st.y * u_time ) / ( 7.0 + u_time ));
-  // } 
+  if ( st.y < .47 ) {
+    if ( topColor.r >=  0.60 ) {
+      topColor = vec4(0.03, 0.13, 0.37, 1.0);
+    }
+ 
+     t = abs( sin( u_time / 10. ));
+  } 
 
-  // if ( st.y < 0.40 ) {
-  //   st.x += ( sin( st.x * u_time ) / ( 7.0  + u_time  ));
-  //   st.y -= ( cos( st.y * u_time ) / ( 23.0 + u_time ));
+  // if ( st.y > .47 ) {
+    // t = abs( sin( u_time / 5. ));
+    // t = .25;
   // }
 
-  color = texture2D( u_background, st );
-  colorB = texture2D( u_foreground, st );
-
-  // if ( st.y > 0.47  ) {
-  //   color = mix(color, colorB, abs( cos( u_time / 20. ))/2.0);
-  // }
-
-
-  // if ( st.y < 0.48 ) {
-  //   color = mix(color, colorB, ( cos( u_time / 25. )));
-  // }
-
+  color = mix( topColor, btmColor, t );
   gl_FragColor = vec4( color );
 
 }
