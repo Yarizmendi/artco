2eb6c5be59de619ebbe097165f5b3370715738ca
yannafaith <hello@yannafaith>
2024-06-30 18:32:01 -0400

    reverts from instance mode

--- comps/P5/InitP5.js
+++ comps/P5/InitP5.js
@@ -1,5 +1,5 @@
 
 export default async function InitP5( sketch, parentRef ) {
-  const p5 = ( await import( "p5" )).default
-  return new p5( async p => sketch( p, parentRef.current ))
+  let p5 = ( await import( "p5" )).default
+  return new p5( sketch, parentRef.current )
 }
