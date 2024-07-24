156172b85eec2ba347c237f339d32afc4cc2be11
yannafaith <hello@yannafaith.com>
2024-07-24 04:01:25 +0000

    bfix

--- app/sketches/[path]/page.tsx
+++ app/sketches/[path]/page.tsx
@@ -3,7 +3,7 @@ import Sketch from "./Sketch.tsx"
 import { getBlob } from "actions/blobs.ts"
 import { stripFileExtension, stripNextPathParams } from "actions/utils.ts"
 
-export default async function Page({ params, children }) {
+export default async function Page({ params }) {
   const prefix = stripNextPathParams( params.path )
   const title = stripFileExtension( params.path )
   const images = await getBlob({ prefix })
