ab34e5b887dead3cc9d0caa55ceec8b3631079de
yannafaith <hello@yannafaith.com>
2024-08-03 21:13:52 +0000

    theme btn refac

--- comps/Nav/ThemeButton.tsx
+++ comps/Nav/ThemeButton.tsx
@@ -5,21 +5,24 @@ import classNames from 'classnames'
 import { DarkModeContext } from '@/hooks/index.ts'
 
 function ThemeButton() {
-    const iconTheme = {
-      common: "bg-gradient-to-r",
-      light: "from-blue-600 via-green-500 to-indigo-400",
-      dark: "dark:from-blue-600 dark:via-yellow-500 dark:to-indigo-400"
+    const { themeClass, setThemeClass} = useContext(DarkModeContext)
+    
+    const handleThemeChange = () => {
+      if (themeClass=="dark") setThemeClass("light")
+      else if (themeClass=="light") setThemeClass("dark")
     }
-    const {isDarkMode, setIsDarkMode} = useContext(DarkModeContext)
+
     return (
       <span 
-        onClick={() =>setIsDarkMode(!isDarkMode)}
+        onClick={handleThemeChange}
         className={classNames(
         "material-symbols-sharp",
         "ml-10 cursor-pointer",
         "inline-block text-transparent bg-clip-text",
-        iconTheme.common, iconTheme.light, iconTheme.dark
-      )}> {isDarkMode?"dark_mode":"light_mode"}
+        "bg-gradient-to-r",
+        "from-blue-600 via-green-500 to-indigo-400",
+        "dark:from-blue-600 dark:via-yellow-500 dark:to-indigo-400",
+      )}> {themeClass=="dark" ? "dark_mode" : "light_mode"}
     </span>
     )
   }
