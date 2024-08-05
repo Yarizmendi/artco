274665dbaa0c7a6be73ed475fc420119ca4d223a
yannafaith <hello@yannafaith.com>
2024-08-05 00:35:09 -0400

    Update page.tsx

--- app/artists/[lastname]/collections/paintings/page.tsx
+++ app/artists/[lastname]/collections/paintings/page.tsx
@@ -2,7 +2,7 @@
 import { getImages } from "actions/images"
 import ArtLink from "@/comps/Links/ArtLinks"
 
-export default async function Homepage() {
+export default async function Page() {
   const images = await getImages()
   return (
     <div className="max-w-[1200px] h-[450px] flex flex-wrap justify-center items-center overflow-auto">
