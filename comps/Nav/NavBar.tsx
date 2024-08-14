b6db664730d0445eb0b638414116098f211a5c05
yannafaith <hello@yannafaith.com>
2024-08-14 06:49:03 +0000

    fixes home btn paths

--- comps/Nav/NavBar.tsx
+++ comps/Nav/NavBar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import classNames from 'classnames'
 import { ThemeButton } from './ThemeButton'
 import { getRoutes } from 'actions/routes'
-import { LogoName } from './LogoName'
+import { LogoName } from './Logo'
 import { ICONS_OUTLINE } from '@/p5/Controls'
 
 async function NavBar() {
