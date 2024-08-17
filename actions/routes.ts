c733c5f43f7208d25c397565021ebda26d454890
yannafaith <hello@yannafaith.com>
2024-08-17 11:59:43 +0000

    fixes route. figures out experimental instrumentation hook does not work in build

--- actions/routes.ts
+++ actions/routes.ts
@@ -1,7 +1,7 @@
 
 const baseNavLinks = [
     { title: "sketches", path: `/${"66bd62276d3999b70d5fd91b"}/sketches`},
-    { title: "prints", path: `/${"66bd62276d3999b70d5fd91b"}/prints`},
+    { title: "prints", path: `/${"66bd62276d3999b70d5fd91b"}/paintings`},
     { title: "paintings", path: `/${"66bd62276d3999b70d5fd91b"}/paintings`}
   ]
 
