9d8f92915fd737ef15d6657abfb09a7506320b87
yannafaith <hello@yannafaith.com>
2024-10-22 21:43:35 -0400

    adds spinning bezier

--- app/[user]/sketches/tests/Test.tsx
+++ app/[user]/sketches/tests/Test.tsx
@@ -152,7 +152,7 @@ export default function PathSKetch({
      
       // topLayer.bezier( p.pmouseX, p.pmouseY, p.mouseX, p.mouseY,  p.pmouseX, p.pmouseY, p.mouseX, p.mouseY)
 
-      // p.rotateZ( drawPlayTimer/ 1000 )
+      p.rotateZ( drawPlayTimer/ 6000 )
       topLayer.bezier(x1, x2, x3, x4, y1, y2, y3, y4, z1, z2, z3, z4)
 
       p.scale(1,-1)
