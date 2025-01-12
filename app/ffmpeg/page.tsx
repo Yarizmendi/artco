b8099aa93a7f656d9d58f655f982a6107942a90c
yannafaith <hello@yannafaith.com>
2025-01-12 09:06:45 -0700

    deployment fix

--- app/ffmpeg/page.tsx
+++ app/ffmpeg/page.tsx
@@ -88,3 +88,11 @@
 //     </button>
 //   );
 // }
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-6xl font-bold">Hello, FFmpeg!</h1>
+    </div>
+  );
+}
