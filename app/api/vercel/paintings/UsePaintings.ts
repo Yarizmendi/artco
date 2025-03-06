6e5d71c0a83766790e44caf694f5bdec4e5077eb
yannafaith <hello@yannafaith.com>
2025-03-05 21:37:35 -0700

    adds uploaderId param to usePaintings

--- app/api/vercel/paintings/UsePaintings.ts
+++ app/api/vercel/paintings/UsePaintings.ts
@@ -3,7 +3,7 @@ import useSWR from "swr"
 
 export function UsePaintings({ uploaderId }) {
   const fetcher = url => fetch(url).then((res) => res.json())
-  const { data, error, isLoading, isValidating, mutate} = useSWR(`/api/vercel/paintings`, fetcher)
+  const { data, error, isLoading, isValidating, mutate} = useSWR(`/api/vercel/paintings/?creatorId=${uploaderId}`, fetcher)
   return {
     data, 
     error, 
--- app/api/vercel/paintings/route.ts
+++ app/api/vercel/paintings/route.ts
@@ -1,8 +1,15 @@
 
-import { getMongoImages } from "actions/images/getImages"
+import { getMongoImages, getMongoImagesByUploaderId } from "actions/images/getImages"
 import { NextRequest, NextResponse } from "next/server"
 
 export async function GET( req: NextRequest ) {
+    const uploaderId = req.nextUrl.searchParams.get("creatorId")
+
+    if (uploaderId) {
+        const data = await getMongoImagesByUploaderId({ uploaderId })
+        return NextResponse.json(data)
+    }
+    
     const data = await getMongoImages()
     return NextResponse.json(data)
 }
